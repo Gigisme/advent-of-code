813f2521371cdc71be850707640ddc9f785c566e
Mindaugas Kulinas <kulinasmindaugas@gmail.com>
2023-09-08 22:33:07 +0300

    third part two

--- third/src/main.rs
+++ third/src/main.rs
@@ -10,9 +10,13 @@ fn main() {
         Ok(lines) => lines,
         Err(e) => panic!("Problem opening file: {}", e)
     };
-    let score:Vec<i32> = part_one(lines);
+    //let score:Vec<i32> = part_one(lines);
+    //let sum:i32 = score.iter().sum();
+    //println!("Part one result: {}", sum);
+    let score:Vec<i32> = part_two(lines);
     let sum:i32 = score.iter().sum();
     println!("{}", sum);
+
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
@@ -21,17 +25,22 @@ fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
     Ok(io::BufReader::new(file).lines())
 }
 
-fn find_first_repeat(first_half: &str, second_half: &str) -> Option<char> {
+fn find_first_repeat(first: &str, second: &str) -> Option<Vec<char>> {
     let mut chars = HashSet::new();
-    for char in first_half.chars() {
+    let mut repeats: Vec<char> = Vec::new();
+    for char in first.chars() {
         chars.insert(char);
     }
-    for char in second_half.chars() {
+    for char in second.chars() {
         if chars.contains(&char) {
-            return Some(char);
+            repeats.push(char);
         }
     }
-    return None;
+    if repeats.len() > 0
+    {
+        return Some(repeats)
+    }
+    return None
 }
 
 fn score_letter(letter: char) -> u8 {
@@ -52,11 +61,45 @@ fn part_one(lines: Lines<BufReader<File>>) -> Vec<i32> {
                 match repeat {
                     None => {}
                     Some(letter) => {
-                        scores.push(score_letter(letter) as i32);
+                        scores.push(score_letter(letter[0]) as i32);
                     }
                 }
             }
             Err(e) => panic!("Problem reading: {}", e)
+        }
+    }
+    scores
+}
+
+fn part_two(lines: Lines<BufReader<File>>) -> Vec<i32> {
+    let mut scores: Vec<i32> = Vec::new();
+    let mut chunk: Vec<String> = Vec::new();
+    for line in lines {
+        match line {
+            Ok(string) => {
+                chunk.push(string);
+            }
+            Err(_) => {}
+        }
+        if chunk.len() == 3 {
+            let repeats = find_first_repeat(&*chunk[0], &*chunk[1]);
+            match repeats {
+                None => {}
+                Some(repeats) => {
+                    let mut letters: String = String::new();
+                    for char in repeats {
+                        letters.push(char);
+                    }
+                    let badge = find_first_repeat(&*chunk[2],&letters);
+                    match badge {
+                        None => {}
+                        Some(letter) => {
+                            scores.push(score_letter(letter[0]) as i32)
+                        }
+                    }
+                }
+            }
+            chunk.clear();
         }
     }
     scores
